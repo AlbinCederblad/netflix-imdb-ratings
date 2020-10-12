827c348df28482192462faf5707bd3c962802b0a
Albin Cederblad <albin.cederblad@hotmail.com>
2020-10-12 11:19:42 +0200

    further updates, some bugs still need to be fixed

--- content.js
+++ content.js
@@ -1,26 +1,84 @@
+function fetchMovieNameYear() {
+  var cardElements = $(".ptrack-content");
+  var anchorElements;
+  if (cardElements) {
+    anchorElements = cardElements.find("a.slider-refocus");
+  } else {
+    return;
+  }
+
+  if (cardElements === undefined || cardElements[0] === undefined) {
+    return;
+  }
+
+  window.sessionStorage.clear();
+
+  for (var i = 0; i < anchorElements.length; i++) {
+    console.log("inside");
+    var title = anchorElements[i].innerText;
+    var jqueryObject = $(anchorElements[i]);
+    var existingIMDBRating = window.sessionStorage.getItem(title);
+    if (existingIMDBRating) {
+      console.log("already exists", existingIMDBRating);
+      var idname = existingIMDBRating.name.replace(/\s/g, '');
+      if (!$('#' + idname).length)
+        $('<div id="' + idname + '">IMDB: ' + existingIMDBRating.name + '</div>').insertAfter(jqueryObject);
+    } else {
+      makeRequestAndAddRating(title, jqueryObject);
+    }
+    title = null;
+    jqueryObject = null;
+    existingIMDBRating = null;
+  }
+}
+
 if (window.sessionStorage !== "undefined") {
-  const target = document.body;
+  var target = document.body;
+  // create an observer instance
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type === 'childList') {
+          console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type === 'attributes') {
+          window.setTimeout(fetchMovieNameYear, 5);
+        }
+    });
+  });
+  // configuration of the observer:
+  var config = {
+    attributes: true,
+    childList: true,
+  };
+  observer.observe(target, config);
+}
 
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
+function makeRequestAndAddRating(name, jqueryObject) {
+  var url = "https://www.omdbapi.com/?apikey=da37df7e&t=" + encodeURI(name);
 
-  // Callback function to execute when mutations are observed
-  const callbackFn = function (mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for (const mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        console.log("A child node has been added or removed.");
-      } else if (mutation.type === "attributes") {
-        console.log(
-          "The " + mutation.attributeName + " attribute was modified."
-        );
-      }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var apiResponse = JSON.parse(xhr.responseText);
+      var imdbRating = apiResponse["imdbRating"];
+      window.sessionStorage.setItem(name, { name: name, imdbRating: imdbRating });
+      var idname = name.replace(/\s/g, '');
+      idname = encodeURI(idname);
+      if (!$('#' + idname).length)
+        $('<div id="' + idname + '">IMDB: ' + imdbRating + '</div>').insertAfter($(jqueryObject));
     }
   };
-
-  // Watch for changes being made to the DOM tree
-  // MutationObserver will invoke specified callback function when DOM changes occur
-  const observer = new MutationObserver(callbackFn);
-
-  observer.observe(target, config);
+  xhr.send();
+}
+function makeNameId($string) {
+    //Lower case everything
+    $string = strtolower($string);
+    //Make alphanumeric (removes all other characters)
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    //Clean up multiple dashes or whitespaces
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    //Convert whitespaces and underscore to dash
+    $string = preg_replace("/[\s_]/", "-", $string);
+    return $string;
 }
